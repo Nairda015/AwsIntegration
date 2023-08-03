6fb1a702f9f01e16dd316b744d7b742c49495486
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-08-03 21:43:11 +0200

    update to private ecr

--- tools/main.tf
+++ tools/main.tf
@@ -1,5 +1,5 @@
 locals {
-  name-prefix   = "${var.aws_owner_login}-${var.env_prefix}-${var.app_name}"
+  name-prefix = "${var.aws_owner_login}-${var.env_prefix}-${var.app_name}"
 }
 
 # ECR Repository
@@ -7,7 +7,7 @@ module "ecr" {
   enabled                 = var.enable_ecr
   source                  = "cloudposse/ecr/aws"
   version                 = "0.38.0"
-  namespace               = "pgs"
+  namespace               = var.aws_owner_login
   stage                   = var.env_prefix
   name                    = var.app_name
   //principals_full_access  = [module.oidc-github.iam_role_arn]
@@ -29,7 +29,7 @@ module "oidc-github" {
   force_detach_policies = true
   github_repositories   = ["${var.repo_owner}/${var.repo_name}"]
   iam_role_name         = "ecr-github-integration"
-  iam_role_policy_arns = [
+  iam_role_policy_arns  = [
     aws_iam_policy.ecr_login_policy.arn,
     aws_iam_policy.ecr_push_policy.arn
   ]
@@ -47,7 +47,7 @@ resource "aws_iam_policy" "ecr_login_policy" {
         "Sid" : "GetAuthorizationToken",
         "Effect" : "Allow",
         "Action" : [
-          "ecr-public:GetAuthorizationToken",
+          "ecr:GetAuthorizationToken",
           "sts:GetServiceBearerToken"
         ],
         "Resource" : "*"
@@ -57,7 +57,7 @@ resource "aws_iam_policy" "ecr_login_policy" {
 }
 resource "aws_iam_policy" "ecr_push_policy" {
   name        = "ECRPushPolicy"
-  description = "ECR write access policy"
+  description = "ECR push policy"
 
   policy = jsonencode({
     "Version" : "2012-10-17",
@@ -66,11 +66,13 @@ resource "aws_iam_policy" "ecr_push_policy" {
         "Sid" : "AllowPush",
         "Effect" : "Allow",
         "Action" : [
-          "ecr-public:BatchCheckLayerAvailability",
-          "ecr-public:CompleteLayerUpload",
-          "ecr-public:InitiateLayerUpload",
-          "ecr-public:PutImage",
-          "ecr-public:UploadLayerPart"
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
         ],
         "Resource" : module.ecr.repository_arn
       }
@@ -81,18 +83,52 @@ resource "aws_iam_policy" "ecr_push_policy" {
 
 # Github Actions Secrets
 resource "github_actions_secret" "aws_owner" {
-  repository       = var.repo_name
-  secret_name      = "AWS_OWNER"
-  plaintext_value  = var.aws_owner_login
+  repository      = var.repo_name
+  secret_name     = "AWS_OWNER"
+  plaintext_value = var.aws_owner_login
 }
 resource "github_actions_secret" "role_to_assume_arn" {
-  repository       = var.repo_name
-  secret_name      = "ROLE_TO_ASSUME_ARN"
-  plaintext_value  = module.oidc-github.iam_role_arn
+  repository      = var.repo_name
+  secret_name     = "ROLE_TO_ASSUME_ARN"
+  plaintext_value = module.oidc-github.iam_role_arn
 }
 resource "github_actions_secret" "ecr_repository_name" {
-  repository       = var.repo_name
-  secret_name      = "ECR_REPOSITORY_NAME"
-  plaintext_value  = module.ecr.repository_name
+  repository      = var.repo_name
+  secret_name     = "ECR_REPOSITORY_NAME"
+  plaintext_value = module.ecr.repository_name
+}
+
+
+# ECR Read User
+resource "aws_iam_user" "ecr_read_user" {
+  name = "ecr_read_user"
+  path = "/"
+  force_destroy = true
+  tags = { "Name" = "${local.name-prefix}-ecr-read-user" }
+}
+
+resource "aws_iam_policy" "ecr_pull_policy" {
+  name        = "ECRPullPolicy"
+  description = "ECR pull policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowPull",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ],
+        "Resource" : module.ecr.repository_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "example_attachment" {
+  user       = aws_iam_user.ecr_read_user.name
+  policy_arn = aws_iam_policy.ecr_pull_policy.arn
 }
 
